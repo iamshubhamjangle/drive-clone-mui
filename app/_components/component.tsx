fca5acfa608a53eb0dfaa8a524a269cf4bdc30e8
Shubham Jangle <jangleshubham13@gmail.com>
2024-01-24 19:42:21 +0530

    feat: layout setup

---
+++ app/_components/component.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import InputBase from "@mui/material/InputBase";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import Paper from "@mui/material/Paper";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import Avatar from "@mui/material/Avatar";
+import Slider from "@mui/material/Slider";
+
+export {
+  Box,
+  Grid,
+  Avatar,
+  AppBar,
+  Toolbar,
+  Typography,
+  Drawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  InputBase,
+  Paper,
+  IconButton,
+  Slider,
+};
